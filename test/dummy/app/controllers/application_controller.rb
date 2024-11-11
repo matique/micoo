88344a753b86160a3d151af53eef3890a90d6bc3
dittmar <dittmar.krall@matique.de>
2024-11-11 10:49:41 +0100

    'minor'

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :always
-  # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   def always
     unless cookies[:cookiesOK] == "x"
