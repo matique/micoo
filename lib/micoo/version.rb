6de7807f5a6367cb9a11a47432cf83b3ead6304e
dittmar <dittmar.krall@matique.de>
2025-06-08 11:27:37 +0200

    'v0.4.0'

--- lib/micoo/version.rb
+++ lib/micoo/version.rb
@@ -1,5 +1,6 @@
 module Micoo
-  VERSION = "0.3.0" # 2024-11-10
+  VERSION = "0.4.0" # 2025-06-08
+  # VERSION = "0.3.0" # 2024-11-10
   # VERSION = "0.2.0" # 2024-05-19
   # VERSION = "0.1.3" # 2024-05-06
 end
